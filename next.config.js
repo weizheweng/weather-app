7bd68319fd49eb3801fd321cead66c308e395edc
wei-zhe <weizheweng@google.com>
2023-06-11 15:54:44 +0800

    1.1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true, // 設定路徑是否要加上尾端的斜線
-  reactStrictMode: true, // 啟用 React 嚴格模式
+    trailingSlash: true, // 設定路徑是否要加上尾端的斜線
+    reactStrictMode: true, // 啟用 React 嚴格模式
+    images: {
+        unoptimized: true,
+    },
 };
 
 
